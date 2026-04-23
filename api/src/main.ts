cf928cba571e11d5b386026359e69875e0a6d56d
Julian Cedaro <julianc@codingit.dev>
2026-04-22 23:46:06 -0300

    set raw cors headers manually

--- api/src/main.ts
+++ api/src/main.ts
@@ -3,11 +3,31 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  console.log('CORS ENABLED - OPEN MODE');
-  app.enableCors({
-    origin: true,
-    credentials: true,
-    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
+
+  app.use((req, res, next) => {
+    const origin = req.headers.origin as string | undefined;
+
+    console.log('RAW CORS HIT:', req.method, req.url, origin);
+
+    if (origin) {
+      res.header('Access-Control-Allow-Origin', origin);
+    } else {
+      res.header('Access-Control-Allow-Origin', '*');
+    }
+
+    res.header('Vary', 'Origin');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS');
+    res.header(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+
+    if (req.method === 'OPTIONS') {
+      return res.sendStatus(204);
+    }
+
+    next();
   });
 
   await app.listen(process.env.PORT ?? 3000);
