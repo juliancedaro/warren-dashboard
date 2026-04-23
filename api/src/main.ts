5b9710532f067ca5f7ec972a997256c84c6bde11
Julian Cedaro <julianc@codingit.dev>
2026-04-22 22:53:39 -0300

    Add cors to main file

--- api/src/main.ts
+++ api/src/main.ts
@@ -3,10 +3,17 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.enableCors({
-    origin: [/localhost:\d+$/],
+    origin: [
+      'http://localhost:5173',
+      /^http:\/\/localhost:\d+$/,
+      /^https:\/\/.*\.vercel\.app$/,
+    ],
     methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+    credentials: true,
   });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
